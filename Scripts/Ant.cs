305988be6ec4e5daf5c60eabef79520a55ac8037
Marcel Klein <33481835+Kleinemann@users.noreply.github.com>
2023-10-06 09:43:57 +0200

    Adding sugar some cleanups

--- Scripts/Ant.cs
+++ Scripts/Ant.cs
@@ -1,20 +1,103 @@
+using AntMe_2_Lib.Definitions;
 using AntMe_2_Lib.Simulator;
 using Godot;
+using System;
 using static AntMe_2_Lib.GameObject.InsectCore;
 
 public partial class Ant: RigidBody3D
 {
 	public AntSimulator SIM;
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+    double cooldown = -1;
+
+    CollisionShape3D ViewArea;
+    MeshInstance3D SignalRing;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
 	{
 		GD.Print("ERSCHIENEN");
+    }
+
+    PlayerColony getColony()
+    {
+        return SIM.PlayerColony;
+    }
+
+
+    Material getPlayerColor()
+    {
+        PlayerColony colony = getColony();
+
+        return GameManager.Instance.PlayerColor[colony.Guid];
+    }
+
+    public override void _EnterTree()
+    {
+        base._EnterTree();
+        ViewArea = GetNode<CollisionShape3D>("AreaView/CollisionView") as CollisionShape3D;
+        SignalRing = GetNode<MeshInstance3D>("AreaView/MeshSignalRing") as MeshInstance3D;
+
+        MeshInstance3D mesh = GetNode<MeshInstance3D>("Mesh") as MeshInstance3D;
+        SignalRing.MaterialOverride = getPlayerColor();
+
+        SignalRing.Scale = new Vector3(SIM.ViewRange, 1, SIM.ViewRange);
+        ViewArea.Scale = new Vector3(SIM.ViewRange, 1, SIM.ViewRange);
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
 	{
+        if(cooldown > 0)
+        {
+            cooldown -= delta;
+            if (cooldown < 0)
+                hide();
+        }
+    }
 
+    public void _on_area_visible_body_3_entered(Node3D body)
+    {
+        SIM.FindsSugar();
+
+        SignalRing.Visible = true;
+        GameManager gm = GameManager.Instance;
+        MeshInstance3D sugar = body.GetParent() as MeshInstance3D;
+        gm.sugars[sugar] -= SIM.Load;
+        gm.updateSugar();
+
+        cooldown = 0.5f;
+    }
+
+    public void hide()
+    {
+        SignalRing.Visible = false;
+    }
+
+    /// <summary>
+    /// Entered a wall and goes back
+    /// </summary>
+    /// <param name="body"></param>
+    public void _on_area_3d_body_entered(Node3D body)
+    {
+        Node3D body3d = (Node3D)body.GetParent();
+
+        float gradA = RotationDegrees.Y;
+        float gradW = body3d.RotationDegrees.Y;
+        float c = 0f;
+        float ausgang = 0f;
+
+        if (gradA >= 0)
+        {
+            c = 90 - gradW - gradA;
+            ausgang = (180 + gradA + (2 * c)) % 360;
+        }
+        else
+        {
+            c = 90 - gradW + gradA;
+            ausgang = (-180 + gradA - (2 * c)) % 360;
+        }
+
+        RotationDegrees = new Vector3(0, ausgang, 0);
     }
 
     internal void Update(double delta)
--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -5,11 +5,10 @@ using AntMe_2_Lib.Simulator;
 using Godot;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Data;
 
 public partial class GameManager : Node
 {
-
 	RandomNumberGenerator rand = new RandomNumberGenerator();
 
     public enum GameStateEnum { NONE, LOADING, RUNNING, PAUSE, END };
@@ -29,13 +28,18 @@ public partial class GameManager : Node
 	DateTime LastUpdate = DateTime.MinValue;
 	int RoundCounter = 1;
 
+	public Dictionary<MeshInstance3D, float> sugars = new Dictionary<MeshInstance3D, float>();
+
     //List<Ant> Ants = new List<Ant>();
-    Dictionary<Guid, BaseMaterial3D> PlayerColor = new Dictionary<Guid, BaseMaterial3D>();
+    internal Dictionary<Guid, BaseMaterial3D> PlayerColor = new Dictionary<Guid, BaseMaterial3D>();
+
+	public static GameManager Instance;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		PrintDebug("Starte Game");
+        Instance = this;
 
         //TODO: Use Only selectet COlonys
         ColonyManager.LoadingUserAntsByDll();
@@ -66,6 +70,8 @@ public partial class GameManager : Node
 				parent.AddChild(hill);
 			}
             GameState = GameStateEnum.RUNNING;
+
+			updateSugar();
         }
 
         if (GameState == GameStateEnum.RUNNING)
@@ -78,7 +84,9 @@ public partial class GameManager : Node
 				PrintDebug($"GAME {GameState} ROUND {RoundCounter}");
 				LastUpdate = DateTime.Now;
 
-				foreach (PlayerColony colony in ColonyManager.Colonys.Values)
+				updateSugar();
+
+                foreach (PlayerColony colony in ColonyManager.Colonys.Values)
 				{
 					BornAnt(colony);
                     colony.DoTicks();
@@ -167,7 +175,6 @@ public partial class GameManager : Node
             }
     }
 
-
     void BornAnt(PlayerColony colony)
 	{
 		AntSimulator sim = ColonyManager.AntBorn(colony, this);
@@ -184,14 +191,14 @@ public partial class GameManager : Node
 		Ants.Add(ant);
 
 		//Material
-		MeshInstance3D mesh = ant.GetNode("MeshInstance3D") as MeshInstance3D;
+		MeshInstance3D mesh = ant.GetNode("Mesh") as MeshInstance3D;
 		mesh.MaterialOverlay = PlayerColor[colony.Guid];
 
 		Node parent = GetParent();
 		parent.AddChild(ant);
 
-		ant.Position = colony.StartPosition.ToGodot() + new Vector3(0,2,0);
-		ant.RotateY(rand.RandfRange(0, 360));
+		ant.Position = colony.StartPosition.ToGodot() + new Vector3(0,2,0);        
+        ant.RotationDegrees = new Vector3(0, rand.RandfRange(0, 360), 0);
 
         ant.SIM.Init();
     }
@@ -200,15 +207,13 @@ public partial class GameManager : Node
 	void StartSimulation()
 	{
 		PrintDebug("Starte Simulation");
+		sugars.Clear();
 
 		int colorIndex = 0;
 
         rand.Seed = GameSettings.GameSeed > 0 ? GameSettings.GameSeed : (ulong)DateTime.Now.Ticks;
 
-		//Camera positions
-		float posX = 0;
-		float posY = 0;
-		float posZ = 0;
+		GD.Print(string.Format("SEED: {0}", rand.Seed));
 
         foreach (PlayerColony colony in ColonyManager.Colonys.Values)
         {
@@ -219,23 +224,7 @@ public partial class GameManager : Node
 			//Coosing StartPosition
 			//TODO: Range of instance position of spawning
 			colony.StartPosition = new System.Numerics.Vector3(rand.RandfRange(-40, 40), 2, rand.RandfRange(-40, 40));
-
-            posX += colony.StartPosition.X;
-			posY += colony.StartPosition.Y;
-			posZ += colony.StartPosition.Z;
         }
-
-        //camera position
-        posX /= ColonyManager.Colonys.Count;
-		posY /= ColonyManager.Colonys.Count;
-		posZ /= ColonyManager.Colonys.Count;
-
-		posY += 10;
-		posZ += 20;
-
-		Camera3D camera = GetParent().GetNode<Camera3D>("Camera3D");
-        camera.Translate(new Vector3(posX, posY, posZ));
-
 
         GameState = GameStateEnum.RUNNING;
 	}
@@ -259,4 +248,47 @@ public partial class GameManager : Node
 
         GD.Print(text != null ? string.Format("{0} => {1}", sender.ToString(), text) : sender.ToString());
 	}
+
+	public void updateSugar()
+	{
+		if(sugars.Count <= ColonyManager.Colonys.Count)
+		{
+			for (var i = sugars.Count; sugars.Count < ColonyManager.Colonys.Count+1; i++)
+			{
+
+				PackedScene ps = ResourceLoader.Load("res://Szenes/Models/sugar.tscn") as PackedScene;
+				MeshInstance3D sugar = ps.Instantiate() as MeshInstance3D;
+
+				sugars.Add(sugar, rand.RandfRange(80f, 100f));
+
+				sugar.Position = new Vector3(rand.RandfRange(-40, 40), 0, rand.RandfRange(-40, 40));
+
+				Node parent = GetParent();
+				parent.AddChild(sugar);
+			}
+        }
+
+		List<MeshInstance3D> rem = new List<MeshInstance3D>();
+        foreach (KeyValuePair<MeshInstance3D, float> pair in sugars)
+        {
+			if (pair.Value <= 0.1f)
+			{
+				rem.Add(pair.Key);
+			}
+			else
+			{
+				float scale = pair.Value * 0.01f;
+				if (scale <= 0.1f)
+					scale = 0.1f;
+                pair.Key.Scale = Vector3.One * scale;
+            }
+        }
+
+		foreach (MeshInstance3D sugar in rem)
+		{
+			sugar.GetParent().RemoveChild(sugar);
+			sugars.Remove(sugar);
+			sugar.QueueFree();
+        }
+    }
 }
